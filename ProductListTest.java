0b42c1047bbb8ebdf815ed0761669240e7f63c77
SankarVinoth <82449682+SankarVinoth@users.noreply.github.com>
2021-05-03 14:50:25 +0530

    Add files via upload

---
+++ ProductListTest.java
@@ -0,0 +1,14 @@
+package in.sankarvinoth;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductListTest {
+
+	@Test
+	void testCase1() {
+		// calling the display method
+		ProductsList.display();  
+}
+}
